85ef459b0ad8c1d4349d26e93257ba00822d7f09
Clinton Akujor <71627543+AClinton27@users.noreply.github.com>
2022-12-12 05:19:31 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
-   * main - prints to the standard error
-   *
-   * Return: one
-   */
+ * main - write to stdout
+ * Return: Always 0
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
