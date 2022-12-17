cdeeb038fd56c3ecb6e7915f8001eb22571020bd
Clinton Akujor <71627543+AClinton27@users.noreply.github.com>
2022-12-17 05:48:44 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,26 +10,22 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+  a = 972
+  b = -98
+  c = 0
+	int largest;
 
-if (a > b)
-{
-if(a > c)
-{
-largest = a;
-}
-else
-{
-largest = c;
-}
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
