92116176824415d842677e3a5a85ff2d7b88d020
Clinton Akujor <71627543+AClinton27@users.noreply.github.com>
2022-12-22 03:43:43 +0100

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,29 @@
 #include "main.h"
 
 /**
- * *cap_string - capitalizes all words of a string.
- *
- * @str: hold character.
- * Return: string.
+ * cap_string - capitalizes all words of a string
+ * @s: string to capitalize
+ * Return: string;
  */
-char *cap_string(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+char *cap_string(char *s)
+{
+	int i = 1;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
 	{
-		if (i == 0)
+		s[0] = s[0] - 32;
+	}
+	for (; s[i]; i++)
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!'
+		    || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '('
+		     || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'
+		     || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
 		{
-			if ((str[i] >= 97 && str[i] <= 122))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
+			s[i] = s[i] - 32;
 		}
 	}
-	return (str);
+	return (s);
 }
